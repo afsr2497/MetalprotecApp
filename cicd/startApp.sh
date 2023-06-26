51fca032622017ec716ec0de04b08c5b3664d635
Alexander Francisco Segovia Razo <xander2497@gmail.com>
2023-06-25 20:06:04 -0500

    [ADD] Code changed

--- cicd/startApp.sh
+++
@@ -1,9 +0,0 @@
-#sudo cp /home/ubuntu/MetalprotecApp/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-#sudo cp /home/ubuntu/MetalprotecApp/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
-
-#sudo systemctl start gunicorn.service
-#sudo systemctl enable gunicorn.service
-
-#sudo service gunicorn restart
-
-#python3 manage.py runserver 0.0.0.0:8000 &
