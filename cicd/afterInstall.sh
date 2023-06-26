41c1ef56167e3f3a10a90a2ba5c6cef11193e318
Alexander Francisco Segovia Razo <xander2497@gmail.com>
2023-06-25 19:40:17 -0500

    [ADD] Config file added for gunicorn

--- cicd/afterInstall.sh
+++ cicd/afterInstall.sh
@@ -1,6 +1,8 @@
 sudo apt install -y python3-pip
 sudo apt install -y virtualenv
+sudo mkdir -pv /var/{log,run}/gunicorn/
+sudo chown -cR ubuntu:ubuntu /var/{log,run}/gunicorn/
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/MetalprotecApp/requirements.txt
-python3 manage.py runserver 0.0.0.0:8000 &
+gunicorn -c /home/ubuntu/MetalprotecApp/config/gunicorn/dev.py
--- cicd/startApp.sh
+++ cicd/startApp.sh
@@ -6,4 +6,4 @@
 
 #sudo service gunicorn restart
 
-python3 manage.py runserver 0.0.0.0:8000 &
+#python3 manage.py runserver 0.0.0.0:8000 &
