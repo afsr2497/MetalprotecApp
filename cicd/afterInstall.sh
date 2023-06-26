09e3ce6dab3035128af5e5f9c3fd1c2eb42a6e76
Alexander Francisco Segovia Razo <xander2497@gmail.com>
2023-06-25 19:24:39 -0500

    [FIX] Development server new configuration added

--- cicd/afterInstall.sh
+++ cicd/afterInstall.sh
@@ -3,3 +3,4 @@ sudo apt install -y virtualenv
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/MetalprotecApp/requirements.txt
+python3 manage.py runserver 0.0.0.0:8000 &
